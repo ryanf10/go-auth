5eae089ec6a89e7f9347348b5e0f16ccf0b1ab06
ryanf10 <ryanfernaldy10@gmail.com>
2023-04-23 14:08:05 +0700

    feat: add migration

---
+++ migration/20230423140317_init.sql
@@ -0,0 +1,28 @@
+-- CreateTable
+CREATE TABLE `user` (
+                        `id` VARCHAR(191) NOT NULL,
+                        `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+                        `email` VARCHAR(191) NOT NULL,
+                        `name` VARCHAR(191) NOT NULL,
+                        `password` VARCHAR(191) NOT NULL,
+                        `roleId` VARCHAR(191) NOT NULL,
+
+                        UNIQUE INDEX `user_email_key`(`email`),
+                        PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `role` (
+                        `id` VARCHAR(191) NOT NULL,
+                        `name` VARCHAR(191) NOT NULL,
+
+                        UNIQUE INDEX `role_name_key`(`name`),
+                        PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `user` ADD CONSTRAINT `user_roleId_fkey` FOREIGN KEY (`roleId`) REFERENCES `role`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- RoleSeeder
+INSERT INTO Role VALUE ('49b1d442-f912-4ef7-aa88-e889be7b6bda ', 'user');
+INSERT INTO Role VALUE ('dec578b7-9785-4acd-9ddc-9a2d2ddabb26', 'admin');
